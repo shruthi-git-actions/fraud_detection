7fc444bba4ebbcabdeee2a1815ca63a53c0d75bb
shruthi-git-actions <85103629+shruthi-git-actions@users.noreply.github.com>
2021-12-20 12:08:47 +0530

    Update fraud_svm_test.py

--- fraud_svm_test.py
+++ fraud_svm_test.py
@@ -36,7 +36,7 @@ test_fpr, test_tpr, te_thresholds = roc_curve(y_test, predictions_SVM)
 print("SVM Accuracy Score -> ",accuracy_score(predictions_SVM, y_test)*100)
 import json
 with open('Output/Accuracy.json', 'w') as f:
-    json.dump((accuracy_score(predictions_SVM, y_test)*100), f)
+    json.dump(("accuracy":accuracy_score(predictions_SVM, y_test)*100), f)
 '''
 plt.plot(test_fpr, test_tpr, label=" AUC TEST ="+str(auc(test_fpr, test_tpr)))
 plt.legend()
@@ -50,4 +50,4 @@ plt.show()
 a=confusion_matrix(y_test,predictions_SVM)
 b=a.tolist()
 with open('Output/Confusion_matrix.json', 'w') as f:
-    json.dump(b, f)
+    json.dump("metrics":b, f)
