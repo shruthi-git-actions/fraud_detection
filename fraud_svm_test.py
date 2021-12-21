88e54685ed206837a483058d85404b583f5ad928
shruthi-git-actions <shruthikumar94@gmail.com>
2021-12-21 16:45:21 +0530

    smhsj

--- fraud_svm_test.py
+++ fraud_svm_test.py
@@ -17,7 +17,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 import pickle
 from sklearn.metrics import confusion_matrix
-
+import json
 
 x_test = pd.read_csv('Data/x_test.csv')
 x_test = x_test.iloc[1: , :]
@@ -32,22 +32,28 @@ with open(Pkl_Filename, 'rb') as file:
 predictions_SVM = SVM.predict(x_test)
 
 test_fpr, test_tpr, te_thresholds = roc_curve(y_test, predictions_SVM)
+print(test_fpr)
+print(test_tpr)
+
 # Use accuracy_score function to get the accuracy
 print("SVM Accuracy Score -> ",accuracy_score(predictions_SVM, y_test)*100)
-import json
+accuracy=accuracy_score(predictions_SVM, y_test)*100
+print(accuracy)
+#a={"Accuracy": accuracy, "fpr": test_fpr,"tpr": test_tpr}
+#b=a.tolist()
+data = {'accuracy':accuracy,'fpr':test_fpr.tolist(),'tpr':test_tpr.tolist()}
+
+
 with open('Output/Accuracy.json', 'w') as f:
-    json.dump((accuracy_score(predictions_SVM, y_test)*100), f)
-'''
+	json.dump(data,f, sort_keys=True, indent=4, separators=(',', ': '))
+    	
+
 plt.plot(test_fpr, test_tpr, label=" AUC TEST ="+str(auc(test_fpr, test_tpr)))
 plt.legend()
 plt.xlabel("True Positive Rate")
 plt.ylabel("False Positive Rate")
 plt.title("AUC(ROC curve)")
-
+plt.savefig('auc.png')
 plot_confusion_matrix(SVM, x_test, y_test, normalize='true')
 plt.show()
-'''
-a=confusion_matrix(y_test,predictions_SVM)
-b=a.tolist()
-with open('Output/Confusion_matrix.json', 'w') as f:
-    json.dump(b, f)
+
